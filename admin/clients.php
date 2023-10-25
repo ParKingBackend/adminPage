ae8cc7d486924ccdeb4603b9368fef3377453caa
ViestursK <95314459+ViestursK@users.noreply.github.com>
2023-10-25 18:16:50 +0300

    Created design for mainPage and Clients

--- admin/clients.php
+++ admin/clients.php
@@ -1,86 +1,93 @@
 <?php
 require('db.php');
-
 ?>
-<link rel="stylesheet" type="text/css" href="css/admin.css">
-<?php
 
-$endpoint = '/api/client/get/all';
-$apiUrl = $apiBaseUrl . $endpoint;
+<!DOCTYPE html>
+<html>
 
-$response = file_get_contents($apiUrl);
+<head>
+    <link rel="stylesheet" type="text/css" href="css/admin.css">
+    <link rel="stylesheet" type="text/css" href="css/pageLayout.css">
+    <link rel="stylesheet" type="text/css" href="css/fonts.css">
+</head>
 
-if ($response === false) {
-    die('Failed to fetch data from the API.'. $response);
-}
+<body>
+    <?php include('topBar.php'); ?>
+    <div class="grid-container">
+        <div class="side-menu">
+            <?php include('sideMenu.php'); ?>
+        </div>
+        <div class="content">
+            <?php
+            $endpoint = '/api/client/get/all';
+            $apiUrl = $apiBaseUrl . $endpoint;
 
-$data = json_decode($response, true);
+            $response = file_get_contents($apiUrl);
 
-if ($data === null) {
-    die('Failed to parse JSON response.');
-}
+            if ($response === false) {
+                die('Failed to fetch data from the API.' . $response);
+            }
 
-// Pagination settings
-$itemsPerPage = 10;
-$totalItems = count($data);
-$totalPages = ceil($totalItems / $itemsPerPage);
+            $data = json_decode($response, true);
 
-// HTML and CSS for displaying data
-echo '<html>';
-echo '<head>';
-echo '<style>';
-echo 'table { border-collapse: collapse; width: 100%; }';
-echo 'table, th, td { border: 1px solid black; }';
-echo 'th, td { padding: 8px; text-align: left; }';
-echo 'th { background-color: #f2f2f2; }';
-echo '.pagination { margin-top: 10px; }';
-echo '</style>';
-echo '</head>';
-echo '<body>';
-echo '<h1>Client Data</h1>';
+            if ($data === null) {
+                die('Failed to parse JSON response.');
+            }
 
-// Pagination logic
-if (isset($_GET['page'])) {
-    $currentPage = min(max(1, $_GET['page']), $totalPages);
-} else {
-    $currentPage = 1;
-}
-$startIndex = ($currentPage - 1) * $itemsPerPage;
-$endIndex = min($startIndex + $itemsPerPage, $totalItems);
+            // Pagination settings
+            $itemsPerPage = 10;
+            $totalItems = count($data);
+            $totalPages = ceil($totalItems / $itemsPerPage);
 
-// Display the table
-echo '<table>';
-echo '<tr>';
-echo '<th><a href="?sort=id&order=asc">ID &#8593;</a> <a href="?sort=id&order=desc">ID &#8595;</a></th>';
-echo '<th>Username</th>';
-echo '<th>Email</th>';
-echo '<th>Bank Account</th>';
-echo '</tr>';
+            echo '<div class="heading"><h1>Clients</h1></div>';
 
-foreach (array_slice($data, $startIndex, $itemsPerPage) as $client) {
-    echo '<tr>';
-    echo '<td>' . $client['id'] . '</td>';
-    echo '<td>' . $client['username'] . '</td>';
-    echo '<td>' . $client['email'] . '</td>';
-    echo '<td>' . $client['bankAccount'] . '</td>';
-    echo '</tr>';
-}
+            // Pagination logic
+            if (isset($_GET['page'])) {
+                $currentPage = min(max(1, $_GET['page']), $totalPages);
+            } else {
+                $currentPage = 1;
+            }
+            $startIndex = ($currentPage - 1) * $itemsPerPage;
+            $endIndex = min($startIndex + $itemsPerPage, $totalItems);
 
-echo '</table>';
+            // Display the table and pagination links inside a div
+            echo '<div class="data-container">';
+            echo '<table>';
+            echo '<tr>';
+            echo '<th><a href="?sort=id&order=asc">ID &#8593;</a> <a href="?sort=id&order=desc">ID &#8595;</a></th>';
+            echo '<th>Username</th>';
+            echo '<th>Email</th>';
+            echo '<th>Bank Account</th>';
+            echo '</tr>';
 
-// Pagination links
-echo '<div class="pagination">';
-if ($totalPages > 1) {
-    for ($page = 1; $page <= $totalPages; $page++) {
-        if ($page === $currentPage) {
-            echo '<span>' . $page . '</span>';
-        } else {
-            echo '<a href="?page=' . $page . '">' . $page . '</a>';
-        }
-    }
-}
-echo '</div>';
+            foreach (array_slice($data, $startIndex, $itemsPerPage) as $client) {
+                echo '<tr>';
+                echo '<td>' . $client['id'] . '</td>';
+                echo '<td>' . $client['username'] . '</td>';
+                echo '<td>' . $client['email'] . '</td>';
+                echo '<td>' . $client['bankAccount'] . '</td>';
+                echo '</tr>';
+            }
 
-echo '</body>';
-echo '</html>';
-?>
+            echo '</table>';
+
+            // Pagination links
+            echo '<div class="pagination">';
+            if ($totalPages > 1) {
+                for ($page = 1; $page <= $totalPages; $page++) {
+                    if ($page === $currentPage) {
+                        echo '<span>' . $page . '</span>';
+                    } else {
+                        echo '<a href="?page=' . $page . '">' . $page . '</a>';
+                    }
+                }
+            }
+            echo '</div>';
+            echo '</div>'; // Closing the table-container div
+            
+            ?>
+        </div>
+    </div>
+</body>
+
+</html>
